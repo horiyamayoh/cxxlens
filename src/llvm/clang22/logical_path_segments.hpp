1d470371f094560e64ce7c2d721387441f48e8d8
horiyamayoh <44720751+horiyamayoh@users.noreply.github.com>
2026-08-16 12:45:48 +0900

    Cover legal trailing two-dot path segments

--- src/llvm/clang22/logical_path_segments.hpp
+++ src/llvm/clang22/logical_path_segments.hpp
@@ -26,6 +26,7 @@ namespace cxxlens::provider::clang22::detail
 
 	static_assert(!contains_parent_path_segment("main.cpp"));
 	static_assert(!contains_parent_path_segment("src/foo..bar.hpp"));
+	static_assert(!contains_parent_path_segment("src/foo.."));
 	static_assert(!contains_parent_path_segment("src/..hidden.hpp"));
 	static_assert(!contains_parent_path_segment("src/hidden..hpp"));
 	static_assert(!contains_parent_path_segment("src/.../main.cpp"));
