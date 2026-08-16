6e49442961ba5a1ef8c2f517f007d7379c367c30
horiyamayoh <44720751+horiyamayoh@users.noreply.github.com>
2026-08-16 12:44:32 +0900

    Cover legal three-dot path segments

--- src/llvm/clang22/logical_path_segments.hpp
+++ src/llvm/clang22/logical_path_segments.hpp
@@ -28,6 +28,7 @@ namespace cxxlens::provider::clang22::detail
 	static_assert(!contains_parent_path_segment("src/foo..bar.hpp"));
 	static_assert(!contains_parent_path_segment("src/..hidden.hpp"));
 	static_assert(!contains_parent_path_segment("src/hidden..hpp"));
+	static_assert(!contains_parent_path_segment("src/.../main.cpp"));
 	static_assert(!contains_parent_path_segment("project://src/.hidden.hpp"));
 	static_assert(!contains_parent_path_segment("..."));
 	static_assert(contains_parent_path_segment(".."));
