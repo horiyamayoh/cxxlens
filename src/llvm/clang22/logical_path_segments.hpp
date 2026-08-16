cdf69f7a43eb004070eae9d37e07c75383406771
horiyamayoh <44720751+horiyamayoh@users.noreply.github.com>
2026-08-16 12:22:53 +0900

    Cover terminal parent path segments

--- src/llvm/clang22/logical_path_segments.hpp
+++ src/llvm/clang22/logical_path_segments.hpp
@@ -33,6 +33,7 @@ namespace cxxlens::provider::clang22::detail
 	static_assert(!contains_parent_path_segment("..."));
 	static_assert(contains_parent_path_segment(".."));
 	static_assert(contains_parent_path_segment("../main.cpp"));
+	static_assert(contains_parent_path_segment("src/.."));
 	static_assert(contains_parent_path_segment("src/../main.cpp"));
 	static_assert(contains_parent_path_segment("src\\..\\main.cpp"));
 	static_assert(contains_parent_path_segment("project://../main.cpp"));
